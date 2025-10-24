8a8023e4ec41b93cb5302e259add784f28e077cd
Hwijung Kim <hwi.meh@gmail.com>
2025-10-24 09:32:45 -0700

    added api call

--- src/components/Class/Class.js
+++ src/components/Class/Class.js
@@ -1,8 +1,24 @@
 import "./Class.css"
-import React, {useState} from "react"
+import React, {useState, useEffect} from "react"
 
 function Class(props) {
     
+    const [classInfo, setClassInfo] = useState({});
+
+    const url = "https://anteaterapi.com/v2/rest/courses/";
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetch(url + props.name)
+            const data = await response.json();
+            console.log(data);
+        }
+
+        
+
+        fetchData()
+    })
+
     return (
         <div className="class">
             <h2>{props.name}</h2>
